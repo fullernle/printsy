2100f241c61568593c0fdb569f9e19a4f893887f
lefuller <fullernle@gmail.com>
2021-07-20 15:22:03 -0700

    added basic search filter

--- frontend/components/search/search_results.jsx
+++ frontend/components/search/search_results.jsx
@@ -1,12 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class SearchResults extends Component {
-	render() {
-		
-		return (
-			<div>
-				Hello.
-			</div>
-		)
-	}
+  componentDidMount() {
+    if (Object.values(this.props.products).length < 1) {
+      this.props.fetchProducts();
+    }
+  }
+
+  fitlerProducts() {
+		const {query, products} = this.props;
+    const productList = Object.values(products);
+		console.log(productList);
+    const results = productList.filter(
+      (ele) => {
+				const name = ele.name.toLowerCase();
+				return name.includes(query);
+			}
+    );
+
+    console.log(results);
+  }
+  render() {
+    return (
+      <div>
+        {this.fitlerProducts()}
+        {this.props.query}
+      </div>
+    );
+  }
 }
