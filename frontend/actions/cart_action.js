0d6e80f708cf643beb991ce3caeaf96d57837850
lefuller <fullernle@gmail.com>
2021-07-22 15:36:44 -0700

    removed console.logs

--- frontend/actions/cart_action.js
+++ frontend/actions/cart_action.js
@@ -38,7 +38,6 @@ export const createCart = (userId) => (dispatch) =>
   );
 
 export const addItemToCart = (cartItem) => (dispatch) => {
-  console.log(cartItem);
   APIUtil.addItemToCart(cartItem).then(
     (cart) => dispatch(receiveCart(cart)),
     (err) => dispatch(receiveErrors(err.responseJSON))
